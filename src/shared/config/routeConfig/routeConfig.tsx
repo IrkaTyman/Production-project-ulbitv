22c159088901c4b4099345f4b5cd639865377d76
Irina <tymanira@mail.ru>
2024-01-04 15:01:02 +0500

    2.21. Несуществующие маршруты

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { AboutPage } from 'pages/AboutPage'
 import { MainPage } from 'pages/MainPage'
+import { NotFoundPage } from 'pages/NotFoundPage'
 import { type RouteProps } from 'react-router-dom'
 
 export enum AppRoutes {
     Main = 'main',
     About = 'about',
+    NotFound = 'not_found'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.Main]: '/',
-    [AppRoutes.About]: '/about'
+    [AppRoutes.About]: '/about',
+    [AppRoutes.NotFound]: '*'
 }
 
 export const routeConfig: RouteProps[] = [
@@ -20,5 +23,9 @@ export const routeConfig: RouteProps[] = [
     {
         path: RoutePath.about,
         element: <AboutPage/>
+    },
+    {
+        path: RoutePath.not_found,
+        element: <NotFoundPage/>
     }
 ]
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import { type FC } from 'react'
+import { classNames } from 'shared/lib/classNames'
+import './Loader.scss'
+
+interface LoaderProps {
+    className?: string
+}
+
+export const Loader: FC<LoaderProps> = ({ className }) => {
+    return (
+        <div className={classNames('lds-ellipsis', {}, [className])}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
