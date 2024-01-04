79e89aa405c4e064f8785781f8c6681695a6b44f
Irina <tymanira@mail.ru>
2024-01-04 13:11:24 +0500

    2.17.  Babel. Extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -41,10 +41,31 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
+    const babelLoader = {
+      test: /\.(js|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            [
+              "i18next-extract", 
+              {
+                locales: ['ru', 'en'],
+                keyAsDefaultValue: true,
+              }
+            ],
+          ]
+        }
+      }
+    }
+
     return [
+        imageLoader,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
-        svgLoader,
-        imageLoader,
     ]
 }
