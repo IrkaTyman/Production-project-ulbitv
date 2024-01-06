8bd46f4fd4a9d08731eb984d3ba56c7fbae6c631
Irina <tymanira@mail.ru>
2024-01-06 13:22:32 +0500

    3.28. add screenshots, ui test's report

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,6 @@
 import { type FC } from 'react'
 import styles from './Navbar.module.scss'
 import { classNames } from 'shared/lib/classNames/classNames'
-import { AppLink } from 'shared/ui/AppLink'
-import { AppLinkTheme } from 'shared/ui/AppLink/AppLink'
-import { useTranslation } from 'react-i18next'
 
 interface NavbarProps {
     className?: string
