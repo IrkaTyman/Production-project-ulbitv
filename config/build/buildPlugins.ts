f0f5c4cebf969ff75f178e229f752fc92e3661d7
Irina <tymanira@mail.ru>
2024-01-06 10:45:55 +0500

    3.27.  fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -20,13 +20,13 @@ export function buildPlugins ({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false
         })
     ]
 
     if (isDev) {
+        plugins.push(new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        }))
         plugins.push(new ReactRefreshWebpackPlugin())
         plugins.push(new webpack.HotModuleReplacementPlugin())
     }
